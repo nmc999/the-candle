561bfad159c6b6c7d0c5fa03e3e676c11af8086e
nmc999 <n.cheng@wykei.org>
2025-11-14 13:09:22 +0800

    Update track-click.js

--- netlify/functions/track-click.js
+++ netlify/functions/track-click.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context) => {
 
     const supabase = createClient(
       process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
+      process.env.SUPABASE_KEY || process.env.SUPABASE_ANON_KEY
     );
 
     // Get current click count
